e8b2e9f7a3592cd6171379ff05fb1f7cd9ab1c7c
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2024-10-08 00:52:57 -0600

    variables

---
+++ Introduccion/src/Constantes.java
@@ -0,0 +1,10 @@
+public class Constantes {
+    public static void main(String[] args) {
+        final var DIAS_SEMANA = 7;
+        System.out.println("DIAS_SEMANA = " + DIAS_SEMANA);
+        //los nombres de constantes van con mayuscula y se separan con Guión bajo
+        final var PI = 3.1416;
+        System.out.println("PI = " + PI);
+        System.out.println("Math.PI" + Math.PI);
+    }
+}
