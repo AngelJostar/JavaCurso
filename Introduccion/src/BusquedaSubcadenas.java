4679f2b5d32ee3e395bd31bb676e0e9c8eda2c0f
AngelJostar <angelrojas@ciencias.unam.mx>
2024-10-09 16:47:04 -0600

    busqueda subcadena

---
+++ Introduccion/src/BusquedaSubcadenas.java
@@ -0,0 +1,9 @@
+public class BusquedaSubcadenas {
+    public static void main(String[] args) {
+        //indexOf - Devuelve el indice de la primera aparicion de la subcadena
+        var cadena1 = "Hola Mundo";
+        //subcadena a buscar "Hola";
+        var indice1 = cadena1.indexOf("Hola");
+        System.out.println("indice1 = " + indice1);
+    }
+}
