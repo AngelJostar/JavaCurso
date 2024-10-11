6c9bcabac41d40c12a7a5384f3ca1ed14e8dcb2b
AngelJostar <angelrojas@ciencias.unam.mx>
2024-10-11 16:41:06 -0600

    formateo de cadenas

--- Introduccion/src/NumerosAleatorios.java
+++ Introduccion/src/NumerosAleatorios.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class NumerosAleatorios {
     public static void main(String[] args) {
-        
+        System.out.println("Numeros Aleatorios");
+        var random = new Random();
+
+        //Generar un numero aleatorio entre 0 y 9
+        var nmeroAleatorio = random.nextInt(10);
+        System.out.println("nmeroAleatorio entre 0 y 9= " + nmeroAleatorio);
+
+        //Generar un numero aleatorio entre 1 y 10
+        nmeroAleatorio = random.nextInt(10) + 1;
+        System.out.println("nmeroAleatorio entre 1 y 10 = " + nmeroAleatorio);
+
+        //Generar un nmero flotante entre 0.0 y 1.0
+        var flotanteAleatorio = random.nextFloat();
+        System.out.println("flotanteAleatorio = " + flotanteAleatorio);
+
+        //simular el lanzamiento de un dado (1 y 6)
+        var dado = random.nextInt(5) + 1;
+        System.out.println("dado = " + dado);
     }
 }
